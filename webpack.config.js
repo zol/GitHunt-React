2cfb857f882d7499f441921a998fcfc6d353e684
Tom Coleman <tom@thesnail.org>
2016-06-02 14:27:41 +1000

    Complete WIP

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,63 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const getConfig = require('hjs-webpack');
 
-module.exports = {
-  entry: './ui/index.js',
-  output: {
-    publicPath: '/'
-  },
-  module: {
-    loaders: [{
-      test: /\.css/,
-      loader: 'style!css'
-    }, {
-      test: /\.js$/,
-      loader: 'babel',
-      exclude: /node_modules/
-    }, {
-      test: /\.json$/,
-      loader: 'json'
-    }]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'ui/index.html'
-    })
-  ],
-  devServer: {
-    proxy: {
-      "/graphql": "http://localhost:3010/graphql",
-      "/login/*": "http://localhost:3010",
-      "/logout": "http://localhost:3010"
-    },
-    historyApiFallback: {
-      index: '/',
-    },
-  },
-}
+// TODO -- sort out a development soln
+const isDev = process.env.NODE_ENV === 'development';
+
+var config = getConfig({
+  isDev: isDev,
+  in: join(src, 'app.js'),
+  out: dest,
+  html: function (context) {
+    return {
+      'index.html': context.defaultTemplate({
+        title: 'yelp-clone from fullstackreact.com',
+        publicPath: isDev ? 'http://localhost:3000/' : '',
+        meta: {
+          'name': 'fullstackreact yelp clone',
+          'description': 'A minimal yelp clone from the team behind the fullstackreact.com book'
+        }
+      })
+    }
+  }
+});
+
+
+//
+//
+//
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
+//
+// module.exports = {
+//   entry: './ui/index.js',
+//   output: {
+//     publicPath: '/'
+//   },
+//   module: {
+//     loaders: [{
+//       test: /\.css/,
+//       loader: 'style!css'
+//     }, {
+//       test: /\.js$/,
+//       loader: 'babel',
+//       exclude: /node_modules/
+//     }, {
+//       test: /\.json$/,
+//       loader: 'json'
+//     }]
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: 'ui/index.html'
+//     })
+//   ],
+//   devServer: {
+//     proxy: {
+//       "/graphql": "http://localhost:3010/graphql",
+//       "/login/*": "http://localhost:3010",
+//       "/logout": "http://localhost:3010"
+//     },
+//     historyApiFallback: {
+//       index: '/',
+//     },
+//   },
+// }
