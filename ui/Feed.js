5bd002dd4cd7e53ee25909ad8d666057136d36b4
Tom Coleman <tom@thesnail.org>
2016-06-21 18:01:00 +1000

    Sort of hacking our way to SSR

--- ui/Feed.js
+++ ui/Feed.js
@@ -136,7 +136,7 @@ const FeedWithData = connect({
           ownProps.params.type.toUpperCase()
         ) || 'TOP',
       },
-      forceFetch: true,
+      forceFetch: false,
     },
   }),
 
--- ui/index.js
+++ ui/index.js
@@ -1,3 +1,4 @@
+require('isomorphic-fetch');
 import createApp from 'saturn-framework/app';
 import routes from './routes';
 
